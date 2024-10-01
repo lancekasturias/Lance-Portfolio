c88bebeac0be53739ea202b41bb6c891e39031a7
lancekasturias <lanceasturias@gmail.com>
2024-10-01 16:38:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-homework-template">
+    <BrowserRouter basename="/Lance-Portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
