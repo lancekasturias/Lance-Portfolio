84b95f010d4d95864f33ea5c6fed6c41e01c6915
lancekasturias <lanceasturias@gmail.com>
2024-10-01 17:57:43 +0800

    update

--- src/components/Shared/SharedLayout.jsx
+++ src/components/Shared/SharedLayout.jsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
 import { Header } from 'components/Header/Header';
-import { HomePage } from 'components/pages/HomePage';
 
 export const SharedLayout = () => {
   return (
